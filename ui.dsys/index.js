f97a2550379c13294b6f61ff3ec90d39571f1be0
akhil <akhilreddy.b@ginnovat.com>
2022-03-30 14:17:50 +0530

    PL Web Design System Project Initial Commit

---
+++ ui.dsys/index.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const fs = require('fs')
+
+const sourceFile = path.join(__dirname, "node", "node_modules", "@paclife", "plds.tokens", "figmagic");
+
+fs.readdir(sourceFile, function(err, files) {
+    console.log(files);
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    files.forEach(function(file) {
+        if (file === 'index.js') return
+        if (file.includes('.js')) {
+            const module = require(`${sourceFile}/${file}`)
+            let lessFileData = ''
+
+            Object.keys(module).forEach((el) => {
+                lessFileData = lessFileData + "\n" + "@" + el + " : " + module[el] + ";"
+            })
+
+            file = file.replace('.js', '')
+            const destinationPath = path.join(__dirname, "..", "ui.apps", "src", "main", "content", "jcr_root", "apps", "plweb-dsys", "clientlibs", "clientlib-site", "css");
+            console.log(destinationPath);
+            fs.writeFile(`${destinationPath}/${file}.less`, lessFileData, (err) => {
+                if (err)
+                    console.log(err);
+                else {
+                    console.log("File written successfully\n");
+                    console.log("The written has the following contents:");
+                    console.log(fs.readFileSync(`${destinationPath}/${file}.less`, "utf8"));
+                }
+            });
+        }
+    });
+});
