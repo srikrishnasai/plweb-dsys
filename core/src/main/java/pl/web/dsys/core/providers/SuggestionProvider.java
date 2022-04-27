b421ef84c363845df75e09fb06b13f3ae7d94900
Vishwa Hirani <vishwa@solvative.com>
2022-04-27 12:44:56 +0530

    "Added lucene index for search suggestion and Suggestion provider class for suggestions."

---
+++ core/src/main/java/pl/web/dsys/core/providers/SuggestionProvider.java
@@ -0,0 +1,17 @@
+package pl.web.dsys.core.providers;
+
+import java.util.List;
+
+import javax.jcr.RepositoryException;
+
+import org.apache.sling.api.resource.ResourceResolver;
+
+
+@org.osgi.annotation.versioning.ProviderType
+public interface SuggestionProvider {
+	List<String> suggest(ResourceResolver resourceResolver, String[] paths, String nodeType, String term, int limit)
+			throws RepositoryException;
+
+	String[] getSearchPaths(ResourceResolver resourceResolver, String searchRootPagePath);
+
+}
