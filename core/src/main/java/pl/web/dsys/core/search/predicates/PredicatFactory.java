415694ed435129fc3d7b9f785d68cdb7934350db
Vishwa Hirani <vishwa@solvative.com>
2022-04-27 17:12:55 +0530

    "Added predicateOption and PredicateFactory classes for suggestion and search result."

--- core/src/main/java/pl/web/dsys/core/search/predicates/PredicatFactory.java
+++ core/src/main/java/pl/web/dsys/core/search/predicates/PredicatFactory.java
@@ -1,5 +1,14 @@
 package pl.web.dsys.core.search.predicates;
 
-public class PredicatFactory {
+import org.apache.sling.api.SlingHttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+@org.osgi.annotation.versioning.ConsumerType
+public interface PredicatFactory {
+    Map<String, String> getRequestPredicate(SlingHttpServletRequest request);
+    List<PredicateOption> getPredicateOptions(SlingHttpServletRequest request);
+    String getTitle();
+    String getName();
     
 }
