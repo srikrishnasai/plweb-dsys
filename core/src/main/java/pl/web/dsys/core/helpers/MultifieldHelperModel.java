c851e7124e5827073d0384ceb0706a83373480d4
Krishna <ksai@ginnovat.com>
2022-05-02 20:12:41 +0530

    Update MultifieldHelperModel.java

--- core/src/main/java/pl/web/dsys/core/helpers/MultifieldHelperModel.java
+++ core/src/main/java/pl/web/dsys/core/helpers/MultifieldHelperModel.java
@@ -1,44 +1,56 @@
 package pl.web.dsys.core.helpers;
 
-import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
-import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.models.annotations.Default;
-import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
-import org.apache.sling.models.annotations.injectorspecific.SlingObject;
-import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-import org.apache.sling.settings.SlingSettingsService;
-import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.resource.ValueMap;
-import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.RequestAttribute;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Model(adaptables = {Resource.class, SlingHttpServletRequest.class},
-defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Model(adaptables = { Resource.class,
+		SlingHttpServletRequest.class }, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class MultifieldHelperModel {
 
 	Logger logger = LoggerFactory.getLogger(MultifieldHelperModel.class);
-	
-	@Inject
+
+	@RequestAttribute
 	private String nodeName = null;
-	
-	@Inject
+
+	@SlingObject
 	public Resource resource;
-	
+
 	public Resource childResource;
-	
+	int length = 0;
+
 	@PostConstruct
 	protected void init() {
-		
 		if (nodeName != null) {
 			childResource = resource.getChild(nodeName);
-		} 
+		}
+	}
 
+	public Resource getChildResource() {
+		return childResource;
+	}
+
+	public int getLength() {
+		List<Resource> items = new ArrayList<>();
+		if (null != childResource && childResource.hasChildren()) {
+			Iterator<Resource> children = childResource.listChildren();
+			while (children.hasNext()) {
+				items.add(children.next());
+			}
+			logger.debug("Items ::{}", items);
+			return items.size();
+		}
+		return length;
 	}
 }
