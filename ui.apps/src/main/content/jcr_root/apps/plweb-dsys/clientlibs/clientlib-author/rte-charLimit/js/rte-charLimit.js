065e46ee02f8a6fb2ecaeef8906a1e66c805becd
Ravi Gadhadara <ragadhadara@irvinecompany.com>
2022-05-24 12:54:09 +0530

    enhancement: Added transform for Secondary Nav, Modified transform for footer and team-gallery, Added remaining char count, Added min width for button

---
+++ ui.apps/src/main/content/jcr_root/apps/plweb-dsys/clientlibs/clientlib-author/rte-charLimit/js/rte-charLimit.js
@@ -0,0 +1,108 @@
+(function (document, $) {
+    "use strict";
+    $(document).on("foundation-contentloaded", function (e) {
+        var $rteContainer = $('.custom-rte').closest('.richtext-container');
+        var $rteBox = $rteContainer.find('.coral-RichText.coral-DecoratedTextfield-input');
+        var maxCharLimit = $('.custom-rte').attr('data-max-char-limit');
+        var ctrlDown = false,
+            ctrlKey = 17,
+            cmdKey = 91,
+            vKey = 86,
+            maxCharCount = maxCharLimit || 250;
+
+        var escapeCodes = [8, 46, 37, 38, 39, 40, 35, 36, 45, 27]; // backspace, delete, leftarrow, uparrow, downarrow, rightarrow, home, end, insert, escape
+
+        function truncateAndUpdate(el, text) {
+            $(el).text(text.substring(0, maxCharCount) || '');
+            $(el).focusEnd();
+        }
+
+        function secondCheckAndChangeText(el) {
+            var text = removeMarkup($(el).text());
+            if (maxCharCount - text.length < 0) {
+                truncateAndUpdate(el, TEXT_VAL);
+            }
+        }
+
+        function removeMarkup(val){
+            var divElem = document.createElement('div');
+            divElem.innerHTML = val;
+            return divElem.textContent;
+        }
+
+        function updateCount(el, text) {
+            $rteContainer.find('.custom-rte__char-count').html(maxCharCount - text.length);
+            secondCheckAndChangeText(el);
+        }
+
+        function addCharCount() {
+            var curCharCount = maxCharCount;
+            if ($rteBox.text()) {
+                curCharCount = $rteBox.text().length;
+            } else {
+                curCharCount = 0;
+            }
+            var $rteCountTextEle = $rteContainer && $rteContainer.find('.custom-rte__char-count');
+            if($rteCountTextEle && $rteCountTextEle.length) {
+                $rteCountTextEle.innerHTML = (maxCharCount - curCharCount);
+            } else {
+                //Charcters counter 
+                $rteContainer.append("<h3><span class='custom-rte__char-count'>" + (maxCharCount - curCharCount) + "</span> chars remaining</h3>");
+            }
+        }
+
+        // ON TEXT INPUT 
+        function onTextInput(e) {
+            var text = removeMarkup($(this).text());
+
+            // on pasting code, check if no of characters is greater than max and truncate if needed
+            if (ctrlDown && e.keyCode == vKey) {
+                truncateAndUpdate(this, $(this).text());
+            }
+
+            // update remaining character letter count
+            updateCount(this, text);
+
+            // prevent default when pressed key is not one of the escape codes, and when the charCount exceeds maxCharcount
+            if (text.length >= maxCharCount && escapeCodes.indexOf(e.keyCode) < 0) {
+                e.preventDefault();
+                return;
+            }
+        }
+
+        addCharCount();
+        $rteBox.on('keydown keyup', onTextInput);
+
+        // focus end jQuery
+        $.fn.focusEnd = function() {
+            $(this).focus();
+            var tmp = $('<span />').appendTo($(this)),
+                node = tmp.get(0),
+                range = null,
+                sel = null;
+        
+            if (document.selection) {
+                range = document.body.createTextRange();
+                range.moveToElementText(node);
+                range.select();
+            } else if (window.getSelection) {
+                range = document.createRange();
+                range.selectNode(node);
+                sel = window.getSelection();
+                sel.removeAllRanges();
+                sel.addRange(range);
+            }
+            tmp.remove();
+            return this;
+        }
+
+        // CHECK ctrl key press
+        $(document).keydown(function (e) {
+            if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = true;
+        }).keyup(function (e) {
+            if (e.keyCode == ctrlKey || e.keyCode == cmdKey) ctrlDown = false;
+        });
+    });
+
+})(document, Granite.$);
+
