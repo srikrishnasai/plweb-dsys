29bd7fad73fc6943edc4fc47bcb269451fe17a6d
Vishwa Hirani <vishwa@solvative.com>
2022-06-07 11:58:57 +0530

    "Customized dialog changes, backend changes and childreneditor.html file for displaying separate auth tag for tab list and accordion components."

---
+++ core/src/main/java/pl/web/dsys/core/pojos/TagInfo.java
@@ -0,0 +1,24 @@
+
+package pl.web.dsys.core.pojos;
+
+public class TagInfo {
+    String tagID;
+    String tagTitle;
+
+    public String getTagID() {
+        return tagID;
+    }
+
+    public void setTagID(String tagID) {
+        this.tagID = tagID;
+    }
+
+    public String getTagTitle() {
+        return tagTitle;
+    }
+
+    public void setTagTitle(String tagTitle) {
+        this.tagTitle = tagTitle;
+    }
+
+}
