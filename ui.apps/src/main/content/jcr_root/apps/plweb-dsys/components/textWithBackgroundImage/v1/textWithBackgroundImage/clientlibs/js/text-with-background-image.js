688dc3401ccbfdb4426aeea69adcdb103d1c4208
Vatsal Mehta <vatsal@solvative.com>
2022-06-13 16:36:33 +0530

    As per window width load background image

--- ui.apps/src/main/content/jcr_root/apps/plweb-dsys/components/textWithBackgroundImage/v1/textWithBackgroundImage/clientlibs/js/text-with-background-image.js
+++ ui.apps/src/main/content/jcr_root/apps/plweb-dsys/components/textWithBackgroundImage/v1/textWithBackgroundImage/clientlibs/js/text-with-background-image.js
@@ -1,9 +1,9 @@
-(function() {
-    var TextWithBackgroundImage = function($el) {
+(function () {
+    var TextWithBackgroundImage = function ($el) {
         /**
          * Sets the position values of the text container
          */
-        this.setTextPos = function() {
+        this.setTextPos = function () {
             if (this.$txtContainer && this.$txtContainer.length) {
                 this.$txtContainer.css({
                     top: this.topVal,
@@ -12,18 +12,43 @@
             }
         };
 
+        // as per window width load image respectively
+        this.loadImage = function () {
+            $('.text-with-bg-img__container').each(function (index, element) {
+                var largeImg = $(this).find('.bg-lg-img').val();
+                var smallImg = $(this).find('.bg-sm-img').val();
+
+                if ($(window).width() < 576) {
+                    $(this).find('.text-with-bg-img__img-container').css("background-image", "url(" + smallImg + ")");
+                }
+                else {
+                    $(this).find('.text-with-bg-img__img-container').css("background-image", "url(" + largeImg + ")");
+                }
+
+            });
+        };
+
         // initializes variables being used in this file
-        this.initVariables = function() {
+        this.initVariables = function () {
             this.$el = $el;
             this.$txtContainer = this.$el.find('.text-with-bg-img__txt-container');
             this.topVal = (this.$el.attr('data-vertical-pos') || 0) + '%';
             this.leftVal = (this.$el.attr('data-horizontal-pos') || 0) + '%';
         };
 
+        this.bindEvents = function () {
+            var _self = this;
+            $(window).resize(function () {
+                _self.loadImage();
+            });
+        };
+
         // init component
-        this.init = function() {
+        this.init = function () {
             this.initVariables();
             this.setTextPos();
+            this.bindEvents();
+            this.loadImage();
         };
 
         if ($el) {
@@ -31,8 +56,8 @@
         }
     };
 
-    $(document).ready(function() {
-        $('.text-with-bg-img').each(function(i) {
+    $(document).ready(function () {
+        $('.text-with-bg-img').each(function (i) {
             if (!$(this).hasClass('text-with-bg-img--mounted')) {
                 $(this).addClass('text-with-bg-img--mounted');
                 new TextWithBackgroundImage($(this));
