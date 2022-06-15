5a7a5253c38d8c740fe0a6aaa74e5892fa32ccc0
Vatsal Mehta <vatsal@solvative.com>
2022-06-15 17:19:13 +0530

    Resolved ui bugs

--- ui.apps/src/main/content/jcr_root/apps/plweb-dsys/clientlibs/clientlib-site/js/global.js
+++ ui.apps/src/main/content/jcr_root/apps/plweb-dsys/clientlibs/clientlib-site/js/global.js
@@ -1,4 +1,21 @@
 
-$(document).ready(function() {
+// For carousel same height for different image sizes and screen dimensions
+function setHeights() {
+	var heights = [];
+	$('.cmp-carousel__item').each(function () {
+		heights.push($(this).height());
+	});
+
+	$('.cmp-carousel__item').height(Math.max(...heights));
+}
+
+$(document).ready(function () {
 	console.log($);
+	setHeights();
+
+	$(window).resize(function () {
+		$('.cmp-carousel__item').height('auto');
+		setHeights();
+	});
+
 });
--- ui.apps/src/main/content/jcr_root/apps/plweb-dsys/components/footer/v1/footer/clientlibs/js/footer.js
+++ ui.apps/src/main/content/jcr_root/apps/plweb-dsys/components/footer/v1/footer/clientlibs/js/footer.js
@@ -1,36 +1,37 @@
 
-(function(){
-    
+(function () {
+
     var UP_ARROW_CLASS = "footer--up-arrow"
-    
+
     var LIST_HEIGHT = 40
 
-    function showList(ulElem , button){
+    function showList(ulElem, button) {
         var maxHeight = $(ulElem).children().length * LIST_HEIGHT;
         var hasFooterUpClass = $('.footer__list').find('.footer--up-arrow');
 
-        if(hasFooterUpClass.length){
+        if (hasFooterUpClass.length) {
             $('.footer--up-arrow').parent().parent().find("ul").css("max-height", "0px");
             $('.footer__list').find('.footer--up-arrow').toggleClass(UP_ARROW_CLASS);
         }
         $(ulElem).css("max-height", maxHeight + "px")
         $(button).toggleClass(UP_ARROW_CLASS);
     }
-    
-    function hideList(ulElem , button){
+
+    function hideList(ulElem, button) {
         $(ulElem).css("max-height", "0px")
-         $(button).toggleClass(UP_ARROW_CLASS);
+        $(button).toggleClass(UP_ARROW_CLASS);
     }
 
-    $(window).on("load", function(){
-        $(".footer__nav-bar").each(function(){
-            $(this).find(".footer__drop-down").each(function(){
-                $(this).on("click", function(){
+    $(window).on("load", function () {
+        $('.footer').parent().css("padding-bottom", $('footer').height() + 'px')
+        $(".footer__nav-bar").each(function () {
+            $(this).find(".footer__drop-down").each(function () {
+                $(this).on("click", function () {
                     var ulElem = $(this).parent().parent().find("ul")
-                    if(!$(this).hasClass(UP_ARROW_CLASS)) {
-                            showList(ulElem, this)
-                        }
-                    else hideList(ulElem , this)
+                    if (!$(this).hasClass(UP_ARROW_CLASS)) {
+                        showList(ulElem, this)
+                    }
+                    else hideList(ulElem, this)
                 })
             })
         })
