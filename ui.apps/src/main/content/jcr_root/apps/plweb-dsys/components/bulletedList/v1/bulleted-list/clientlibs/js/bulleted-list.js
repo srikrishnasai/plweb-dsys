dcd94848f304e6b66118029f6cd02692592353e0
Mansi Shah <mansi.shah@pacificlife.com>
2022-06-20 15:29:10 +0530

    bullet-list-bug-fix

--- ui.apps/src/main/content/jcr_root/apps/plweb-dsys/components/bulletedList/v1/bulleted-list/clientlibs/js/bulleted-list.js
+++ ui.apps/src/main/content/jcr_root/apps/plweb-dsys/components/bulletedList/v1/bulleted-list/clientlibs/js/bulleted-list.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    
+    // Add Dynamic row for div
     $('.bulleted-list').find('.bulleted-list-wrapper').each(function () {
         var length = $(this).attr("data-length");
         var columnCount = $(this).attr('data-columnCount');
@@ -6,13 +8,11 @@ $(document).ready(function () {
         if(columnCount == 'two'){
             rawCount=length/2;
             rawCount=Math.ceil(rawCount);
-            console.log("rawCount",rawCount);
             $(this).find('.bullet-list').css('grid-template-rows', 'repeat('+rawCount+',auto)');
         }
         else if(columnCount == 'three'){
             rawCount=length/3;
             rawCount=Math.ceil(rawCount);
-            console.log("rawCount",rawCount);
             if(length !== "4"){
                 $(this).find('.bullet-list').css('grid-template-rows', 'repeat('+rawCount+',auto)');
             }
@@ -20,5 +20,15 @@ $(document).ready(function () {
                 $(this).find('.bullet-list').css('grid-auto-flow', 'row');
             }
         }
+    });
+
+    //remove default style
+    $('.bulleted-list').each(function() {
+       var hasCheckmarClass = $(this).hasClass('cmp-list--checkmarks');
+       var hasNumberClass = $(this).hasClass('cmp-list--numbers');
+
+       if(hasCheckmarClass || hasNumberClass){
+            $(this).find('.bullet-list').removeClass('bullet-list-bullet');
+       }
     });
 });
