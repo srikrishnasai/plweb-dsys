b8d45671c5c4f5c7e414ade2b064f26425ae7ca0
Vatsal Mehta <vatsal@solvative.com>
2022-06-23 16:40:18 +0530

    Footer overlap issue resolved and remove style option of accordion

--- ui.apps/src/main/content/jcr_root/apps/plweb-dsys/clientlibs/clientlib-site/js/global.js
+++ ui.apps/src/main/content/jcr_root/apps/plweb-dsys/clientlibs/clientlib-site/js/global.js
@@ -2,20 +2,22 @@
 // For carousel same height for different image sizes and screen dimensions
 function setHeights() {
 	var heights = [];
-	$('.cmp-carousel__item').each(function () {
-		heights.push($(this).height());
-	});
-
-	$('.cmp-carousel__item').height(Math.max(...heights));
+	setTimeout(function () {
+		$('.cmp-carousel').each(function () {
+			var $this = $(this);
+			heights = [];
+			$this.find('.cmp-carousel__item').each(function () {
+				heights.push($(this).height());
+			});
+			$(this).find('.cmp-carousel__item').height(Math.max(...heights));
+		});
+	}, 1500);
 }
 
-$(document).ready(function () {
-	console.log($);
+$(window).on("load", function () {
 	setHeights();
-
 	$(window).resize(function () {
 		$('.cmp-carousel__item').height('auto');
 		setHeights();
 	});
-
 });
