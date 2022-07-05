968957bfe89777f79e0e992fd12c96d9d57f1797
Krishna <ksai@ginnovat.com>
2022-07-05 13:41:47 +0530

    Header Model Junit

--- core/src/test/java/pl/web/dsys/core/models/HeaderModelTest.java
+++ core/src/test/java/pl/web/dsys/core/models/HeaderModelTest.java
@@ -1,11 +1,16 @@
 package pl.web.dsys.core.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.apache.sling.api.resource.Resource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+import com.day.cq.wcm.api.Page;
 
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
@@ -15,24 +20,37 @@ import pl.web.dsys.core.base.DsysTestContext;
 class HeaderModelTest {
 
 	private final AemContext context = DsysTestContext.newAemContext();
-	public static final String HEADER_COMPONENT = "/content/headerComponent";
+	public static final String HEADER_COMPONENT = "/content/home/jcr:content/headerComponent";
+
+	@Mock
+	Page page;
 
 	@BeforeEach
 	public void setUp() {
 		context.load().json("/headerModel.json", "/content");
-		context.currentPage("/content");
+		context.currentPage("/content/home");
 	}
 
 	@Test
 	void doTestHeaderModel() {
 		HeaderModel header = getHeaderModelUnderTest(HEADER_COMPONENT);
-		assertEquals("/content/dam/we-retail/en/activities/hiking-camping/Hiking-Campaign-Male.jpg", header.getFileReference());
-		assertEquals("/content/plweb-dsys/us/en/misc/all-sites", header.getAllSitesPath());
+		assertEquals("/content/dam/we-retail/en/activities/hiking-camping/Hiking-Campaign-Male.jpg",
+				header.getFileReference());
+		assertEquals("/content/home", header.getAllSitesPath());
 		assertEquals("Pacific Life Advisory", header.getTopNavHeaderText());
 		assertEquals("true", header.getHideSearch());
 		assertEquals("true", header.getHideTopNav());
 		assertEquals("/content/plweb-dsys/home/search-results.html", header.getSearchTargetPath());
-		assertEquals("/content/home.html", header.getHomePagePath());
+		assertNotNull(header.getPrimarylinks());
+		assertEquals("Home", header.getPrimarylinks().get(0).getLinkText());
+		assertEquals("/content/home", header.getPrimarylinks().get(0).getTargetPath());
+		assertEquals("/content/home/home.html", header.getHomePagePath());
+		assertEquals("_blank", header.getPrimarylinks().get(0).getLinkTarget());
+		assertNull(header.getPrimarylinks().get(0).getDescription());
+		assertEquals("/content/experience-fragments/plweb-dsys/us/en/site/secondary-nav/second-header/jcr:content",
+				header.getPrimarylinks().get(0).getSecondaryNavXfPath());
+		assertNull(header.getPrimarylinks().get(0).getIconPath());
+		assertNotNull(header.getAllSites());
 	}
 
 	private HeaderModel getHeaderModelUnderTest(String resourcePath) {
